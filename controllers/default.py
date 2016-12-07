59d601bd1d499e856540d96c845af8c4ff89d186
scm <smendonc@gmail.com>
2016-12-06 16:58:07 -0800

    *slightly* better print page

--- controllers/default.py
+++ controllers/default.py
@@ -99,19 +99,21 @@ def user():
 @auth.requires_login()
 def print_page():
     import json
-    voting_choices = []
+    voting_choices = {}
     election_votes = json.loads(db(db.votes_races.user_email == auth.user.email).select(db.votes_races.votes_json).first().votes_json)
     for idx, vote in enumerate(election_votes):
         if vote is not None:
             race = db(db.races.id == idx).select().first()
-            vote = ' '.join(vote)
-            voting_choices.append(race.office + ": " + vote)
+            vote = ''.join(vote)
+            #voting_choices.append(race.office + ": " + vote)
+            voting_choices[race.office] = vote
 
     measure_votes = json.loads(db(db.votes_measures.user_email == auth.user.email).select(db.votes_measures.votes_json).first().votes_json)
     for idx, vote in enumerate(measure_votes):
         if vote is not None:
             race = db(db.measures.id == idx).select().first()
-            voting_choices.append(race.letter + ": " + vote)
+            #voting_choices.append(race.letter + ": " + vote)
+            voting_choices[race.letter] = vote
 
     return dict(voting_choices = voting_choices)
 
